495ecefc5b0b72e35313a4ac56a69aae5a3663f4
Stefan Hendriks <stefan@kaizenbv.nl>
2022-02-15 15:59:38 +0100

    Add pseudo code with mocking

--- platforms/python/test.py
+++ platforms/python/test.py
@@ -1,9 +1,32 @@
+from unittest.mock import MagicMock
 import unittest
 
 
+class ExampleClass:
+    """A simple example class"""
+
+    someValue = "Default value"
+
+    def __init__(self, some_value):
+        self.someValue = some_value
+        # empty constructor
+
+    def foo(self):
+        return self.someValue
+
+
 class MyTestCase(unittest.TestCase):
+
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        thing = ExampleClass("Foo")
+        self.assertEqual("Foo", thing.foo())
+
+        thing.foo = MagicMock(return_value="Bar")
+        # Call function
+        self.assertEqual("Bar", thing.foo())
+
+        # Assert that it has been called
+        thing.foo.assert_called()
 
 
 if __name__ == '__main__':
