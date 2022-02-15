4c0f6273641df852f0fd527ecd6b0ef9fbce86fc
Arjen van der Ende <arjenvanderende@users.noreply.github.com>
2022-02-15 15:56:24 +0100

    Add helper to fetch all cards from JSON file

--- platforms/golang/main.go
+++ platforms/golang/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/pkg/errors"
+)
 
 func main() {
 	fmt.Printf("foo = %d", foo())
@@ -8,4 +15,24 @@ func main() {
 
 func foo() int {
 	return 4
+}
+
+func fetchAllCards() ([]interface{}, error) {
+	var result []interface{}
+	path := "../../katas/MTGCardReader/allcards.json"
+
+	body, err := ioutil.ReadFile(path)
+	if err != nil {
+		return result, errors.Wrapf(err, "unable to open file: %s", path)
+	}
+
+	// remove byte-order-mark (BOM)
+	body = bytes.TrimPrefix(body, []byte("\xef\xbb\xbf"))
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return result, errors.Wrapf(err, "unable to parse card JSON")
+	}
+
+	return result, nil
 }
--- platforms/golang/main_test.go
+++ platforms/golang/main_test.go
@@ -13,3 +13,26 @@ func TestFoo(t *testing.T) {
 		assert.Equal(t, 4, foo)
 	})
 }
+
+func TestFetchAllCards(t *testing.T) {
+
+	t.Run("returns data", func(t *testing.T) {
+		data, err := fetchAllCards()
+
+		assert.NoError(t, err)
+		assert.Equal(t, 17449, len(data))
+		assert.Equal(t, map[string]interface{}{
+			"BasicType":        float64(4),
+			"Cmc":              float64(5),
+			"ColorIdentity":    float64(1),
+			"ImageRelativeUrl": "/cards/normal/en/mir/51.jpg?1496448326",
+			"Legality":         float64(408),
+			"ManaCost":         "{4}{W}",
+			"MultiverseId":     float64(0),
+			"Name":             "Zuberi, Golden Feather",
+			"Price":            float64(0.1),
+			"Rating":           float64(0),
+			"Types":            "Legendary Creature — Griffin",
+		}, data[0])
+	})
+}
